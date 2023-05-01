70b7e6d6e3461031eecac0f7c8075841a8f2f02c
1337m3rcy <memeslam1337@bk.ru>
2023-05-01 17:08:55 +0400

    Update SQLQuery.ts

--- src/application/modules/DB/SQLQuery.ts
+++ src/application/modules/DB/SQLQuery.ts
@@ -1,7 +1,8 @@
-import { Database, Statement } from "sqlite3";
+import { Client } from "pg";
+
 
 export default class SQLQuery{
-    constructor(readonly db: Database, private query: string = '', private values: any [] = []){
+    constructor(readonly db: Client, private query: string = '', private values: any [] = []){
     }
 
     conditions(conditions: object, operand: string = 'AND'): SQLQuery{
@@ -30,26 +31,54 @@ export default class SQLQuery{
         return this;
     }
 
-    all<T>(){
-        return new Promise<T>((resolve) => {
-            this.db.all(this.query,this.values,
-                (error: Error, rows: any) => resolve(error ? [] : rows))
-        });
+    // all<T>(){
+    //     return new Promise<T>((resolve) => {
+    //         this.db.all(this.query,this.values,
+    //             (error: Error, rows: any) => resolve(error ? [] : rows))
+    //     });
+    // }
+
+    async all<T>() {
+        try {
+            const rows = await this.db.query(this.query, this.values);
+            return rows;
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
     }
 
-    get<T>(){
-        return new Promise<T | null>((resolve) => {
-            this.db.get(this.query,this.values,
-                (error: Error, row: any) => resolve(error ? null : row))
-        });
+    // get<T>(){
+    //     return new Promise<T | null>((resolve) => {
+    //         this.db.get(this.query,this.values,
+    //             (error: Error, row: any) => resolve(error ? null : row))
+    //     });
+    // }
+
+    async get<T>() {
+        try {
+            const rows = await this.db.query(this.query, this.values);
+            return rows;
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
     }
 
-    run(){
-        return new Promise<boolean>((resolve) => {
-            this.db.run(this.query,this.values,
-                (error: Error) => resolve(!error))
-        });
+    // run(){
+    //     return new Promise<boolean>((resolve) => {
+    //         this.db.run(this.query,this.values,
+    //             (error: Error) => resolve(!error))
+    //     });
+    // }
+
+    async run() {
+        try {
+            const rows = await this.db.query(this.query, this.values);
+            return rows;
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
     }
-
-
 }
